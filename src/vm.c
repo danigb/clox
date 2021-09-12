56e47df3b61caeb58b30af01e91d474d7254100d
danigb <danigb@gmail.com>
2021-09-12 19:04:49 +0200

    19.4.1 Concatenation

--- src/vm.c
+++ src/vm.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdarg.h>
+#include <string.h>
+
 #include "common.h"
 #include "debug.h"
+#include "object.h"
+#include "memory.h"
 #include "compiler.h"
 #include "vm.h"
 
@@ -27,6 +31,20 @@ static Value peek(int distance)
 static bool isFalsey(Value value)
 {
     return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
+}
+static void concatenate()
+{
+    ObjString *b = AS_STRING(pop());
+    ObjString *a = AS_STRING(pop());
+
+    int length = a->length + b->length;
+    char *chars = ALLOCATE(char, length + 1);
+    memcpy(chars, a->chars, a->length);
+    memcpy(chars + a->length, b->chars, b->length);
+    chars[length] = '\0';
+
+    ObjString *result = takeString(chars, length);
+    push(OBJ_VAL(result));
 }
 
 static void resetStack()
@@ -124,8 +142,25 @@ static InterpretResult run()
             BINARY_OP(BOOL_VAL, <);
             break;
         case OP_ADD:
-            BINARY_OP(NUMBER_VAL, +);
+        {
+            if (IS_STRING(peek(0)) && IS_STRING(peek(1)))
+            {
+                concatenate();
+            }
+            else if (IS_NUMBER(peek(0)) && IS_NUMBER(peek(1)))
+            {
+                double b = AS_NUMBER(pop());
+                double a = AS_NUMBER(pop());
+                push(NUMBER_VAL(a + b));
+            }
+            else
+            {
+                runtimeError(
+                    "Operands must be two numbers or two strings.");
+                return INTERPRET_RUNTIME_ERROR;
+            }
             break;
+        }
         case OP_SUBTRACT:
             BINARY_OP(NUMBER_VAL, -);
             break;
