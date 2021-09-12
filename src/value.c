e67047141e0e67dc2b3c2303b2bdd9f9819fada8
danigb <danigb@gmail.com>
2021-09-12 18:10:07 +0200

    18 Types of Values

--- src/value.c
+++ src/value.c
@@ -33,5 +33,5 @@ void freeValueArray(ValueArray *array)
 
 void printValue(Value value)
 {
-    printf("%g", value);
+    printf("%g", AS_NUMBER(value));
 }
--- src/vm.c
+++ src/vm.c
@@ -1,10 +1,56 @@
 #include <stdio.h>
+#include <stdarg.h>
 #include "common.h"
 #include "debug.h"
 #include "compiler.h"
 #include "vm.h"
 
 VM vm;
+
+void push(Value value)
+{
+    *vm.stackTop = value;
+    vm.stackTop++;
+}
+
+Value pop()
+{
+    vm.stackTop--;
+    return *vm.stackTop;
+}
+
+static Value peek(int distance)
+{
+    return vm.stackTop[-1 - distance];
+}
+
+static void resetStack()
+{
+    vm.stackTop = vm.stack;
+}
+
+static void runtimeError(const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    vfprintf(stderr, format, args);
+    va_end(args);
+    fputs("\n", stderr);
+
+    size_t instruction = vm.ip - vm.chunk->code - 1;
+    int line = vm.chunk->lines[instruction];
+    fprintf(stderr, "[line %d] in script\n", line);
+    resetStack();
+}
+
+void initVM()
+{
+    resetStack();
+}
+
+void freeVM()
+{
+}
 
 /* 
  * For do/while trick read: https://craftinginterpreters.com/a-virtual-machine.html#binary-operators
@@ -13,12 +59,17 @@ static InterpretResult run()
 {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
-#define BINARY_OP(op)     \
-    do                    \
-    {                     \
-        double b = pop(); \
-        double a = pop(); \
-        push(a op b);     \
+#define BINARY_OP(valueType, op)                        \
+    do                                                  \
+    {                                                   \
+        if (!IS_NUMBER(peek(0)) || !IS_NUMBER(peek(1))) \
+        {                                               \
+            runtimeError("Operands must be numbers.");  \
+            return INTERPRET_RUNTIME_ERROR;             \
+        }                                               \
+        double b = AS_NUMBER(pop());                    \
+        double a = AS_NUMBER(pop());                    \
+        push(valueType(a op b));                        \
     } while (false)
 
     for (;;)
@@ -46,19 +97,24 @@ static InterpretResult run()
             break;
         }
         case OP_ADD:
-            BINARY_OP(+);
+            BINARY_OP(NUMBER_VAL, +);
             break;
         case OP_SUBTRACT:
-            BINARY_OP(-);
+            BINARY_OP(NUMBER_VAL, -);
             break;
         case OP_MULTIPLY:
-            BINARY_OP(*);
+            BINARY_OP(NUMBER_VAL, *);
             break;
         case OP_DIVIDE:
-            BINARY_OP(/);
+            BINARY_OP(NUMBER_VAL, /);
             break;
         case OP_NEGATE:
-            push(-pop());
+            if (!IS_NUMBER(peek(0)))
+            {
+                runtimeError("Operand must be a number.");
+                return INTERPRET_RUNTIME_ERROR;
+            }
+            push(NUMBER_VAL(-AS_NUMBER(pop())));
             break;
         case OP_RETURN:
         {
@@ -72,20 +128,6 @@ static InterpretResult run()
 #undef BINARY_OP
 #undef READ_CONSTANT
 #undef READ_BYTE
-}
-
-static void resetStack()
-{
-    vm.stackTop = vm.stack;
-}
-
-void initVM()
-{
-    resetStack();
-}
-
-void freeVM()
-{
 }
 
 InterpretResult interpret(const char *source)
@@ -105,16 +147,4 @@ InterpretResult interpret(const char *source)
     InterpretResult result = run();
     freeChunk(&chunk);
     return result;
-}
-
-void push(Value value)
-{
-    *vm.stackTop = value;
-    vm.stackTop++;
-}
-
-Value pop()
-{
-    vm.stackTop--;
-    return *vm.stackTop;
 }
