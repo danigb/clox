6e6e08456ec9ad07166429d4fced71f8e9112995
danigb <danigb@gmail.com>
2021-09-12 14:05:59 +0200

    14.4 Disassembling Chunks

--- src/main.c
+++ src/main.c
@@ -1,6 +1,16 @@
 #include "common.h"
+#include "chunk.h"
+#include "debug.h"
 
 int main(int argc, const char *argv[])
 {
+    Chunk chunk;
+    initChunk(&chunk);
+    writeChunk(&chunk, OP_RETURN);
+
+    dissasembleChunk(&chunk, "Test");
+
+    freeChunk(&chunk);
+
     return 0;
 }
